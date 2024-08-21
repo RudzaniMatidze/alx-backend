936479be11ffb5170fe89ca13514fef64e2d4d93
RudzaniMatidze <rdzmatidze@gmail.com>
2024-08-21 12:30:03 +0000

    added else statement

--- 0x01-caching/2-lifo_cache.py
+++ 0x01-caching/2-lifo_cache.py
@@ -26,12 +26,15 @@ class LIFOCache(BaseCaching):
             key (str): The key under which the item should be stored.
             item (any): The item to be stored in the cache.
         """
-        if len(self.cache_data) >= BaseCaching.MAX_ITEMS \
-                and key not in self.cache_data.keys():
-            last_key, last_value = self.cache_data.popitem()
-            print("DISCORD: {}".format(last_key))
+        if key is None or item is None:
+            pass
+        else:
+            if len(self.cache_data) >= BaseCaching.MAX_ITEMS \
+                    and key not in self.cache_data.keys():
+                last_key, last_value = self.cache_data.popitem()
+                print("DISCORD: {}".format(last_key))
 
-        self.cache_data[key] = item
+            self.cache_data[key] = item
 
     def get(self, key):
         """
