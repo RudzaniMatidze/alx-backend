9447e565944685b1f63811c5503048614754e151
RudzaniMatidze <rdzmatidze@gmail.com>
2024-08-27 18:02:42 +0000

    TASK: 1. Basic Babel setup

--- 0x02-i18n/1-app.py
+++ 0x02-i18n/1-app.py
@@ -17,7 +17,7 @@ class Config(object):
     BABEL_DEFAULT_TIMEZONE = 'UTC'
 
 
-#configure the flask app
+# configure the flask app
 app = Flask(__name__)
 app.config.from_object(Config)
 app.url_map.strict_slashes = False
